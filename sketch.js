97966c30c84baa375251f70b6f1d1102750d06f9
Nicolas34110 <94319305+Nicolas34110@users.noreply.github.com>
2022-11-25 18:04:53 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var canvas;
+var backgroundImage;
+var bgImg;
+var database;
+var form, player;
+var playerCount;
+var allPlayers;
+var gameState;
+var car1, car2, car1Img, car2Img;
+var cars=[];
+var track;
+
+function preload() {
+  backgroundImage = loadImage("./assets/planodefundo.png");
+  car1Img = loadImage("./assets/car1.png");
+  car2Img = loadImage("./assets/car2.png");
+  track = loadImage("./assets/PISTA.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw() {
+  background(backgroundImage);
+  if(playerCount===2){
+    game.update(1);
+  }
+  if(gameState===1){
+    game.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
